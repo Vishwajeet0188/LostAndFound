dbfc123394106ad4a62f6063bc2de8f952ebe061
Vishwajeet Kumar <vishwajeetku0107@gmail.com>
2026-01-12 01:44:32 +0530

    Fix duplicate MongoStore import and session store

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
-const MongoStore = require("connect-mongo");
 
 const Item = require("./models/item");
 const User = require("./models/user");          
